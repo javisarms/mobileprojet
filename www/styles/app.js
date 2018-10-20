234949b6388bec64dfd9ac432afed61df041291d
Javi Sarmiento <javisarmiento1@gmail.com>
2018-10-20 15:47:09 +0200

    update 1

--- www/styles/app.js
+++ www/styles/app.js
@@ -1,5 +1,6 @@
 window.fn = {};
 
+// Open and load pages for splitter etc.
 window.fn.open = function() {
   var menu = document.getElementById('menu');
   menu.open();
@@ -12,22 +13,61 @@ window.fn.load = function(page) {
     .then(menu.close.bind(menu));
 };
 
-document.addEventListener('init', function(event) {
-  var page = event.target;
 
-  if (page.id === 'main') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('event.html', {data: {title: 'Event'}});
-    };
-  } else if (page.id === 'event') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
-});
+// Pusing and popping pages
+function push(page) {
+  document.querySelector('#myNavigator').pushPage(page, {data: {title: 'Event'}});
+}
 
+window.fn.pop = function() {
+  var content = document.getElementById('myNavigator');
+  content.popPage();
+};
+
+// Showing modals
 function showModal() {
   var modal = document.querySelector('ons-modal');
-  modal.show();
-  setTimeout(function() {
-    modal.hide();
-  }, 2000);
+  modal.show({ animation: 'fade' });
 }
+
+function closeModal() {
+  var modal = document.querySelector('ons-modal');
+  modal.hide({ animation: 'fade' });
+}
+
+// Pull to refresh
+ons.ready(function() {
+  var pullHook = document.getElementById('pull-hook');
+
+  pullHook.addEventListener('changestate', function(event) {
+    var message = '';
+
+    switch (event.state) {
+      case 'initial':
+        message = 'Pull to refresh';
+        break;
+      case 'preaction':
+        message = 'Release';
+        break;
+      case 'action':
+        message = 'Loading...';
+        break;
+    }
+
+    pullHook.innerHTML = message;
+  });
+
+  pullHook.onAction = function(done) {
+    setTimeout(done, 1000);
+  };
+});
+
+// Login
+var login = function() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  if (email != 'bob' && password != 'secret') {
+    ons.notification.alert('Incorrect username or password.');
+  }
+};
